768908fc841cdfeab5493d279b191ebb14929276
Vardan Safaryan <safarianvardan@gmail.com>
2025-01-05 15:08:46 +0400

    fix(CHORE): reverted config file

--- src/config/config.db.ts
+++ src/config/config.db.ts
@@ -1,9 +1,9 @@
 export const CONFIG_DB = {
     dialect: 'mysql',
-    host: 'localhost',
+    host: 'mysql',
     port: 3306,
     username: 'root',
-    password: '',
+    password: 'root',
     database: 'si-core',
     autoLoadModels: true,
     synchronize: true,
