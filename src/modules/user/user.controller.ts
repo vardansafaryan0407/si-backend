6c550713a57cdaf36f8b1864023cbe803c6cca22
cr7hardwork <741230real@gmail.com>
2025-01-23 11:46:33 +0300

    fix controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,42 +1,28 @@
-import {Body, Controller, Get,Param,Patch,Post,Request, UseGuards} from "@nestjs/common";
-import {UserService} from "./user.service";
-import {User} from "./user";
-import {AuthGuard} from "src/core/guards/auth.guard";
+import { Controller, Get, Post, Put, Request, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user';
+import { AuthGuard } from 'src/core/guards/auth.guard';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
 
-    constructor(private userService: UserService) {
-    }
+  @UseGuards(AuthGuard)
+  @Get('')
+  public async getCurrentUser(@Request() req) {
+    const userId = req.user.id;
 
-    @UseGuards(AuthGuard)
-    @Get('')
-    public async getCurrentUser(@Request() req) {
-     const userId = req.user.id 
-     return this.userService.findOne(userId)
+    return this.userService.findOne(userId);
+  }
 
-    }
+  @Get('list')
+  async getAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
 
+  @Post('')
+  public async createUser() {}
 
-    @Get('list')
-    async getAll(): Promise<User[]> {
-        return this.userService.findAll();
-    }
-
-
-    @Get(':id')
-    async getById(@Param('id') id: number) {
-      return this.userService.getById(id);
-    }
-
-    @Post('')
-    public async createUser() {
-    }
-
-
-    @Patch(':id')
-    async updateCurrentUser(@Param('id') id: number, @Body() userData: User) : Promise<User> {
-      return this.userService.update(id, userData);
-    }
-    }
-
+  @Put('')
+  public async updateCurrentUser() {}
+}
