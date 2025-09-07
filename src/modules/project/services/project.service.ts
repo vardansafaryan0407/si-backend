13f26118a94556274669f9369f63df3b95fd3cd9
Vardan Safaryan <safarianvardan@gmail.com>
2025-09-07 18:25:43 +0400

    fix(PROJECT): fixed no position search

--- src/modules/project/services/project.service.ts
+++ src/modules/project/services/project.service.ts
@@ -131,7 +131,6 @@ export class ProjectService extends BaseService<Project> {
 
     const joinMembers: any = {
       model: ProjectPosition,
-      required: true,
       include: [
         {
           model: Skill,
